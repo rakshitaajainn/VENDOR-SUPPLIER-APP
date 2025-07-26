1ee0c82ec10c2417c6d0c6394f957a640a810450
Divay123321 <divaygoyal92@gmail.com>
2025-07-26 12:51:56 +0530

    Update user.js

---
+++ vendor-supplier-app/models/user.js
@@ -0,0 +1,25 @@
+// models/user.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    mobile: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['vendor', 'supplier'], required: true },
+    // You can add more fields like address, business name, etc.
+    createdAt: { type: Date, default: Date.now }
+});
+
+// Hash password before saving
+UserSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+module.exports = mongoose.model('User', UserSchema);
