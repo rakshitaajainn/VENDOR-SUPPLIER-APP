2668175efe28f6d0267c1d925500481959f7b2fa
rakshitaajainn <rakshita.9788@gmail.com>
2025-07-26 23:03:31 +0530

    Update products.js

--- vendor-supplier-app/backend/routes/products.js
+++ vendor-supplier-app/backend/routes/products.js
@@ -1,1 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const productController = require('../controllers/productController');
+const { protect } = require('../middleware/authMiddleware');
+
+router.route('/')
+    .get(protect, productController.getAllProducts)
+    .post(protect, productController.createProduct);
+
+router.route('/:id')
+    .get(protect, productController.getProduct)
+    .put(protect, productController.updateProduct)
+    .delete(protect, productController.deleteProduct);
+
+router.route('/:id/reviews')
+    .post(protect, productController.createProductReview);
+
+router.get('/top', productController.getTopProducts);
+
+module.exports = router;
 
